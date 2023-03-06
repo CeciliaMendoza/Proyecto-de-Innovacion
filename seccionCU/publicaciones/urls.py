89e6c86ca2089c9c867d2283eca795cdc2682323
Cecilia <al192895@alumnos.uacj.mx>
2023-03-05 22:41:17 -0700

    publicaciones

--- seccionCU/publicaciones/urls.py
+++ seccionCU/publicaciones/urls.py
@@ -3,5 +3,7 @@ from publicaciones import views
 
 urlpatterns = [
     path("", views.principal, name="home"),
-    path("search/", views.busquedas, name = "buquedas")
+    path("search/", views.busquedas, name = "buquedas"),
+    path("categoria/<int:categoria_id>/", views.categoria, name="categoria"),
+
 ]
--- seccionCU/publicaciones/views.py
+++ seccionCU/publicaciones/views.py
@@ -1,9 +1,40 @@
 from django.shortcuts import render
+from .models import Categoria, Publicaciones
+from django.core.paginator import Paginator
 
 # Create your views here.
 
 def principal(request):
-    return render(request, "publicaciones/home.html")
+
+    publicaciones_all = Publicaciones.objects.all()
+    categorias = Categoria.objects.all()
+    # Show 5 contacts per page.
+    paginator = Paginator(publicaciones_all, 5) 
+    #obtiene el numeor de pagina 
+    page_number = request.GET.get('page')
+    publicaciones = paginator.get_page(page_number)
+
+    return render(request, "publicaciones/home.html", {"publicaciones" : publicaciones, "categorias" : categorias})
+
+def categoria(request, categoria_id):
+    publicaciones_all = Publicaciones.objects.all()
+    categorias = Categoria.objects.all()
+    # Show 5 contacts per page.
+    paginator = Paginator(publicaciones_all, 5) 
+    #obtiene el numeor de pagina 
+    page_number = request.GET.get('page')
+    publicaciones = paginator.get_page(page_number)
+
+    return render(request, "publicaciones/home.html", {"publicaciones" : publicaciones, "categorias" : categorias})
 
 def busquedas(request):
-    return render(request, "publicaciones/index.html")
+    #doesnt work yet
+    publicaciones_all = Publicaciones.objects.all()
+    categorias = Categoria.objects.all()
+    # Show 5 contacts per page.
+    paginator = Paginator(publicaciones_all, 5) 
+    #obtiene el numeor de pagina 
+    page_number = request.GET.get('page')
+    publicaciones = paginator.get_page(page_number)
+
+    return render(request, "publicaciones/home.html", {"publicaciones" : publicaciones, "categorias" : categorias})
