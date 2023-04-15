6ea3dddb855a0931d1488e7b6ecbca03b8548789
MarcoBetance0327 <marco_a_bp@outlook.com>
2023-04-14 20:58:10 -0600

    Dark Mode

---
+++ seccionCU/publicaciones/static/publicaciones/assets/js/dark-mode.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    const prefiereDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
+
+    // console.log(prefiereDarkMode.matches);
+
+    if(prefiereDarkMode.matches) {
+        document.body.classList.add('dark-mode');
+    } else {
+        document.body.classList.remove('dark-mode');
+    }
+
+    prefiereDarkMode.addEventListener('change', function() {
+        if(prefiereDarkMode.matches) {
+            document.body.classList.add('dark-mode');
+        } else {
+            document.body.classList.remove('dark-mode');
+        }
+    });
+    
+    const btnDarkMode = document.querySelector('.dark-mode-button');
+
+    btnDarkMode.addEventListener('click', function(){
+        document.body.classList.toggle('dark-mode');
+    });
+});
+
